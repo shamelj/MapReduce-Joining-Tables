208a1a1993695321e5aa91f07d24a335b59ae5d9
Shamel Juidy <70355802+shamelj@users.noreply.github.com>
2022-03-29 18:37:39 +0300

    Refactoring

--- JoinTables.java
+++ JoinTables.java
@@ -1,65 +1,67 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class JoinTables {
-	public static class CustomerMapper extends
-			Mapper<Object, Text, Text, Text> {
+	public static class CustomerMapper extends Mapper<Object, Text, Text, Text> {
 		public void map(Object key, Text value, Context context)
 				throws IOException, InterruptedException {
-			String input = "customer,"+value.toString();
-			String[] tuple = input.split(",");// Tuple = ['customer',Customer ID, First Name, Last Name]
+			String input = "c," + value.toString();
+			String[] tuple = input.split(",");// Tuple = ['c',Customer, ID, First Name, Last Name]
 			context.write(new Text(tuple[1]), new Text(input));
 		}
 	}
+
 	public static class TransactionMapper extends
-	Mapper<Object, Text, Text, Text> {
-	public void map(Object key, Text value, Context context)
-		throws IOException, InterruptedException {
-	String input = "transaction,"+value.toString();
-	String[] tuple = input.split(",");// Tuple = ['customer',Transaction ID, Date, Customer ID, Amount]
-	context.write(new Text(tuple[3]), new Text(input));
-}
-}
+			Mapper<Object, Text, Text, Text> {
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String input = "t," + value.toString();
+			String[] tuple = input.split(",");// Tuple = ['t', Customer ID, Transaction ID, Date, Amount]
+			context.write(new Text(tuple[1]), new Text(input));
+		}
+	}
 
 	public static class JoinTablesReducer extends
 			Reducer<Text, Text, Text, Text> {
-		private IntWritable result = new IntWritable();
-
-		public void reduce(Text key, Iterable<Text> values,
-				Context context) throws IOException, InterruptedException {
+		public void reduce(Text key, Iterable<Text> values, Context context)
+				throws IOException, InterruptedException {
 			ArrayList<String[]> customers = new ArrayList<>();
-			ArrayList<String[]> transactions = new ArrayList<>();		
+			ArrayList<String[]> transactions = new ArrayList<>();
 			for (Text val : values) {
 				String input = val.toString();
 				String[] tuple = input.split(",");
-				if (tuple[0].equals("customer"))
+				if (tuple[0].equals("c"))
 					customers.add(tuple);
 				else
 					transactions.add(tuple);
 			}
-			String joinedTable = "";
-			for( String[] customer : customers)
-				for (String[] transaction: transactions){
-					String joinedTuples = Arrays.toString(customer)+ Arrays.toString(transaction);
-					joinedTable+=joinedTuples+"\n";
+			for (String[] customer : customers)
+				for (String[] transaction : transactions) {
+					String tuple = concate(customer, transaction);
+					context.write(new Text(""), new Text(tuple));
 				}
+
 			
-			context.write(new Text("*"), new Text(joinedTable));
 		}
+	}
+
+	private static String concate(String[] customer, String[] transaction) {
+		String tuple = "[";
+		for (int i = 1; i < customer.length; i++)
+			tuple += customer[i] + ", ";
+		for (int i = 2; i < transaction.length; i++)
+			tuple += transaction[i]
+					+ ((i == transaction.length - 1) ? "" : ", ");
+		tuple += "]";
+		return tuple;
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -67,8 +69,11 @@ public class JoinTables {
 		Job job = Job.getInstance(conf, "word count");
 		job.setJarByClass(JoinTables.class);
 		job.setReducerClass(JoinTablesReducer.class);
-		org.apache.hadoop.mapreduce.lib.input.MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CustomerMapper.class);
-		org.apache.hadoop.mapreduce.lib.input.MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, TransactionMapper.class);
+		org.apache.hadoop.mapreduce.lib.input.MultipleInputs.addInputPath(job,
+				new Path(args[0]), TextInputFormat.class, CustomerMapper.class);
+		org.apache.hadoop.mapreduce.lib.input.MultipleInputs.addInputPath(job,
+				new Path(args[1]), TextInputFormat.class,
+				TransactionMapper.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
 		FileOutputFormat.setOutputPath(job, new Path(args[2]));
